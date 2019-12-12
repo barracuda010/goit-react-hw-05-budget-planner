317ef0bb84a432074d206ee2fdbc16d582356111
Viktor Mryga <dontworryweareinsafe@gmail.com>
2019-12-12 17:34:27 +0200

    Updates

---
+++ precache-manifest.1122a631bcdecfdf21eec2d6c318d842.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3beb5b19d375de1065bda176dd2eee",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "730584c3097aee4338cf",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "2f7991aa37e3fc5c1f2a",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.e91b2e85.chunk.js"
+  },
+  {
+    "revision": "730584c3097aee4338cf",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.45166d27.chunk.js"
+  },
+  {
+    "revision": "b23f4a51e2275a9e0cee",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.08401fe7.js"
+  }
+]);
